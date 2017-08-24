866fa583f34d996e0db6ebd26219c85e82b64bcd
JASON STIESI <jason@JASONs-MacBook-Pro-2.local>
2017-08-23 20:25:21 -0400

    Initial Commit

---
+++ How Many/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  How Many
+//
+//  Created by JASON STIESI on 8/23/17.
+//  Copyright © 2017 JASON STIESI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
