2666d7a460e94110f2fc0ad946333c4881919c0b
Jason <jstiesi12683@icloud.com>
2017-08-23 20:50:07 -0400

    Simple guessing game
    simple guessing game, guess how many fingers the computer is holding up

--- How Many/ViewController.swift
+++ How Many/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fingersTextField: UITextField!
+    
+    @IBAction func guess(_ sender: AnyObject) {
+        
+        let diceRoll = String(arc4random_uniform(6))
+        
+        if fingersTextField.text == diceRoll {
+            
+            resultLabel.text = "You are right"
+            
+        } else {
+            
+            resultLabel.text = "Wrong! it was a " + diceRoll + " "
+            
+        }
+        
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
